03cfda3def3402ab172dff7ca46cfb295bb3bf58
sumtwelve <sumtwelve@gmail.com>
2023-03-01 17:05:18 -0700

    basic index.js, tests and utils folder

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("huh");
+const inquirer = require("inquirer");
+
+console.log("Welcome to Sumtwelve's Team Page Generator!");
+console.log("\nA series of prompts will guide you through creating a webpage\nto store useful info and links about your team members.");
+
+inquirer
+    .prompt(
+        {
+            type: "input",
+            message: "What should we call your team?",
+            name: "teamName"
+        }
+    )
+    .then((answers) => {
+        // TODO: handle the responses
+    })
+    .catch((error) => {
+        console.error(error);
+    });
