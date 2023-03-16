28f66a8d0a55ab5938a7a5a6d86eff571b5c7c9c
sumtwelve <sumtwelve@gmail.com>
2023-03-15 23:53:26 -0600

    first draft of HTML generator, have yet to test

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -12,45 +12,6 @@ function generateHTML(team) {
     <h1>${team.teamName}</h1>
     <section class="cards-section">
         ${generateCards(team)}
-        <article class="card manager">
-            <div class="card-header">
-                <h3>Stacy Rodriguez</h3>
-                <h4>Manager</h4>
-            </div>
-            <div class="card-body">
-                <ul>
-                    <li>ID: 329328</li>
-                    <li>Email Address: <a target="_blank" href="mailto:srodriguez@gmail.com">srodriguez@gmail.com</a></li>
-                    <li>Office Number: 45</li>
-                </ul>
-            </div>
-        </article>
-        <article class="card engineer">
-            <div class="card-header">
-                <h3>Percy Ibanez</h3>
-                <h4>Engineer</h4>
-            </div>
-            <div class="card-body">
-                <ul>
-                    <li>ID: 766584</li>
-                    <li>Email Address: <a target="_blank" href="mailto:pibanez@gmail.com">pibanez@gmail.com</a></li>
-                    <li>GitHub Username: <a target="_blank" href="https://github.com/Pulsar">Pulsar</a></li>
-                </ul>
-            </div>
-        </article>
-        <article class="card intern">
-            <div class="card-header">
-                <h3>Hailey Alberts</h3>
-                <h4>Intern</h4>
-            </div>
-            <div class="card-body">
-                <ul>
-                    <li>ID: 954433</li>
-                    <li>Email Address: <a target="_blank" href="mailto:halberts@gmail.com">halberts@gmail.com</a></li>
-                    <li>School: The University of Utah</li>
-                </ul>
-            </div>
-        </article>
     </section>
 </body>
 </html>`;
@@ -58,8 +19,42 @@ function generateHTML(team) {
 }
 
 
+
+// This function is similar to one found in index.js
+// I wanted to avoid triple-nesting the loop this time, and I also made use of the class methods.
 function generateCards(team) {
-    
+    // step 1: break `team` object into an array of its values and iterate over it
+    for (let memberList of Object.values(team)) {
+        // step 2: the first value in `team` is teamName, which we don't want to iterate over. Skip it by testing if it's a string.
+        if (typeof memberList !== "string") {
+            // step 3: Iterate over each element of the member lists, which are instances of the role classes (Manager, Engineer, or Intern)
+            for (let member of memberList) {
+                // getPrintableInfo() returns an array of the member data
+                let memberDataArr = member.getPrintableInfo();
+                let name = memberDataArr[0];
+                let id = memberDataArr[1];
+                let email = memberDataArr[2];
+                let roleSpecific = memberDataArr[3]; // for Manager, office number. Engineer, GitHub username. For Intern, school.
+                let role = member.getRole();
+
+                // Now that we have our data, we can return a finished HTML card.
+                // Note: getRoleSpecificDataField() returns the NAME of the role specific data. Manager -> "Office Number". Engineer -> "GitHub Username", Intern -> "School"
+                return `<article class="card ${role.toLowerCase()}">
+                <div class="card-header">
+                    <h3>${name}</h3>
+                    <h4>${role}</h4>
+                </div>
+                <div class="card-body">
+                    <ul>
+                        <li>ID: ${id}</li>
+                        <li>Email Address: <a target="_blank" href="mailto:${email}">${email}</a></li>
+                        <li>${member.getRoleSpecificDataField()}: ${roleSpecific}</li>
+                    </ul>
+                </div>
+            </article>`;
+            }
+        }
+    }
 }
 
 module.exports = generateHTML;
