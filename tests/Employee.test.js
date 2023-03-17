1751eb44a74e7f2e2471e35f24c1b72f500a7244
sumtwelve <sumtwelve@gmail.com>
2023-03-16 22:26:48 -0600

    began work on jests, removed getPrintableInfo() from classes

---
+++ tests/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/employee');
+
+describe('Employee', () => {
+    describe('getName', () => {
+        it('should return this.name', () => {
+            
+        })
+    })
+})
